06eb79c14b679ef229f677d6130dbbcbd18a60e8
jayz666 <jasonhenly07@aol.com>
2024-08-28 09:13:53 +0100

    Update config.lua
    Enhanced Configuration Options:
    Notification Settings:
    Added options to choose between okokNotify, ESX default notifications, and QBCore notifications.
    Configurable notification duration (NotificationDuration).
    Configurable notification message (Message).
    Health Management:
    Added settings to define the health threshold (HurtThreshold) below which the player is considered hurt.
    Configurable health reduction amount (HealthReduction) and interval (ReductionInterval).
    Defined the health recovery threshold (RecoveryThreshold) above which the player is considered recovered.
    Visual Effects:
    Options to enable or disable screen effects (EnableScreenEffect) and camera shake (EnableCameraShake).
    Configurable screen effect type (ScreenEffect) and camera shake intensity (CameraShakeIntensity).

--- Startup_Injured/config.lua
+++ Startup_Injured/config.lua
@@ -1,15 +1,29 @@
+Config = {
+    -- Notification Settings
+    Notifications = {
+        UseOKOKNotify = true,         -- Use okokNotify for notifications
+        UseESXDefaultNotify = false,  -- Use ESX notification system
+        UseQBCoreNotify = false,      -- Use QBCore notification system
+        NotificationDuration = 5000,  -- Duration of the notification in milliseconds
+        Message = "You are hurt, go to the hospital to get treated.", -- Message to be displayed
+    },
 
+    -- Health Management
+    Health = {
+        HurtThreshold = 160,          -- Health threshold below which the player is considered hurt
+        HealthReduction = 3,          -- Amount of health reduced per interval when hurt
+        ReductionInterval = 60000,    -- Time interval (in milliseconds) for health reduction
+        RecoveryThreshold = 161,      -- Health level above which the player is considered recovered
+    },
 
-Config = {}
-
-Config.OKOKNotify = true -- OKOKNotify or UseESXDefaultNotify
-
-Config.UseESXDefaultNotify = false -- OKOKNotify or UseESXDefaultNotify
-
-
--- Translation
-
-Config.Message = "You are hurt, go to the hospital to get treated."
+    -- Visual Effects
+    VisualEffects = {
+        EnableScreenEffect = true,    -- Enable screen effect when hurt
+        ScreenEffect = 'Rampage',     -- Screen effect to use
+        EnableCameraShake = true,     -- Enable camera shake when hurt
+        CameraShakeIntensity = 0.12,  -- Intensity of the camera shake
+    }
+}
 
 
 
